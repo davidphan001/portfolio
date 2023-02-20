7dbef4e70fec3c313a6f59479717f3de0c2dab9d
David Phan <dphan@companionprotect.com>
2023-02-20 03:35:17 -0600

    updating to a string

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from "./App";
 
 test("renders david link", () => {
   render(<App />);
-  const linkElement = screen.getByText(david);
+  const linkElement = screen.getByText("David");
   expect(linkElement).toBeInTheDocument();
 });
